16c864d66a799bccade9707b4c6ea05e7e3fe014
Martin Sawilla <martin.sawilla@gmail.com>
2021-10-19 11:56:04 +0200

    chore: setup clean micronaut-camunda installation

---
+++ pizza-service/README.md
@@ -0,0 +1,20 @@
+## Micronaut 3.1.1 Documentation
+
+- [User Guide](https://docs.micronaut.io/3.1.1/guide/index.html)
+- [API Reference](https://docs.micronaut.io/3.1.1/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/3.1.1/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+## Feature assertj documentation
+
+- [https://assertj.github.io/doc/](https://assertj.github.io/doc/)
+
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+## Feature camunda documentation
+
+- [https://github.com/camunda-community-hub/micronaut-camunda-bpm](https://github.com/camunda-community-hub/micronaut-camunda-bpm)
+
---
+++ pizza-service/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("io.micronaut.application") version "2.0.6"
+}
+
+version = "0.1"
+group = "info.novatec"
+
+repositories {
+    mavenCentral()
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("info.novatec.*")
+    }
+}
+
+dependencies {
+    annotationProcessor("io.micronaut:micronaut-http-validation")
+    implementation("info.novatec:micronaut-camunda-bpm-feature:2.1.0")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("javax.annotation:javax.annotation-api")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.camunda.bpm.assert:camunda-bpm-assert:13.0.0")
+    implementation("io.micronaut:micronaut-validation")
+
+}
+
+
+application {
+    mainClass.set("info.novatec.Application")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+    targetCompatibility = JavaVersion.toVersion("17")
+}
+
+
+
