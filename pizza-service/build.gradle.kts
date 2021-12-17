110e9bbd889115164f870fd85d0c11e5ee1d8aab
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-12-17 08:02:12 +0100

    chore: Dependency updates and set Java 8 compatibility

--- pizza-service/build.gradle.kts
+++ pizza-service/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "2.0.6"
+    id("io.micronaut.application") version "3.0.2"
 }
 
 version = "0.1"
@@ -21,26 +21,21 @@ micronaut {
 
 dependencies {
     annotationProcessor("io.micronaut:micronaut-http-validation")
-    implementation("info.novatec:micronaut-camunda-bpm-feature:2.2.0")
+    implementation("info.novatec:micronaut-camunda-bpm-feature:2.3.2")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut:micronaut-validation")
     implementation("javax.annotation:javax.annotation-api")
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.camunda.bpm.assert:camunda-bpm-assert:13.0.0")
-    implementation("io.micronaut:micronaut-validation")
-
 }
-
 
 application {
     mainClass.set("info.novatec.Application")
 }
 java {
-    sourceCompatibility = JavaVersion.toVersion("17")
-    targetCompatibility = JavaVersion.toVersion("17")
+    sourceCompatibility = JavaVersion.toVersion("8")
+    targetCompatibility = JavaVersion.toVersion("8")
 }
-
-
-
