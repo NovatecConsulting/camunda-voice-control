3446e663cc0f80c33955c5cde68bb659bae33ec0
Martin Sawilla <martin.sawilla@gmail.com>
2021-12-22 11:38:41 +0100

    feat: customer_closed process now works with alexa

--- pizza-service/src/main/java/info/novatec/PizzaDeliveryProcessInstanceCreator.java
+++ pizza-service/src/main/java/info/novatec/PizzaDeliveryProcessInstanceCreator.java
@@ -4,8 +4,8 @@ import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.runtime.server.event.ServerStartupEvent;
 import jakarta.inject.Singleton;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import org.camunda.bpm.engine.RuntimeService;
 
 @Singleton
@@ -17,14 +17,14 @@ public class PizzaDeliveryProcessInstanceCreator implements
 
   public PizzaDeliveryProcessInstanceCreator(RuntimeService runtimeService) {
     this.runtimeService = runtimeService;
-    orders.add("2x Salami");
-    orders.add("3x Funghi");
-    orders.add("1x Speziale");
+    orders.add("eine gro\u00dfe Pizza Salami");
+    //orders.add("3x Funghi");
+    //orders.add("1x Speziale");
   }
 
   @Override
   public void onApplicationEvent(ServerStartupEvent event) {
-    orders.forEach(order -> this.runtimeService.startProcessInstanceByKey("pizza_delivery", Map.of("order", order)));
-
+    orders.forEach(order -> this.runtimeService.startProcessInstanceByKey("pizza_delivery",
+        Collections.singletonMap("order", order)));
   }
 }
